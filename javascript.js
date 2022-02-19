b0d4732c0161b1b5922a22edcbfdaf5b88d10131
Antônio Moraes <96862273+28r@users.noreply.github.com>
2022-02-19 15:56:33 +0900

    First working version

---
+++ javascript.js
@@ -0,0 +1,80 @@
+async function GetWeatherData(location) {
+    let apistring = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=5a25c78aa652856a65886422d6707dbe";
+    try {
+        const response = await fetch(apistring, {mode: "cors"});
+        return response.json();
+    } catch(error) {
+        console.error(`Error. For reference: "${error}"`);
+    }
+}
+
+function MainFunction(location, forc) {
+    GetWeatherData(location).then(WeatherData => {
+        console.log(WeatherData);
+        EmptyResults();
+        let cityName = WeatherData.name;
+        AddToDOM('cityName', cityName);
+        let country = WeatherData.sys.country;
+        AddToDOM('country', country);
+        let weatherDescription = WeatherData.weather[0].description;
+        AddToDOM('weatherdescription', weatherDescription);
+        let weatherMain = WeatherData.weather[0].main;
+        AddToDOM('weathermain', weatherMain);
+        let temp = WeatherData.main.temp;
+        let tempmin = WeatherData.main.temp_min;
+        let tempmax = WeatherData.main.temp_max;
+        if (forc === 'C') {
+            temp = KelvinToC(temp);
+            tempmin = KelvinToC(tempmin);
+            tempmax = KelvinToC(tempmax);
+        }
+        else if (forc === 'F') {
+            temp = KelvinToF(temp);
+            tempmin = KelvinToF(tempmin);
+            tempmax = KelvinToF(tempmax);
+        }
+        else {
+            temp = KelvinToC(temp);
+            tempmin = KelvinToC(tempmin);
+            tempmax = KelvinToC(tempmax);
+        }
+        AddToDOM('temp', temp);
+        AddToDOM('tempmin', tempmin);
+        AddToDOM('tempmax', tempmax);
+        let displayarea = document.getElementById('weatherdata');
+        displayarea.classList.remove('invisible');
+    });
+}
+
+function AddToDOM(id, variable) {
+    let area = document.getElementById(id);
+    let result = document.createElement('span');
+    result.classList.add('result');
+    result.innerHTML = variable;
+    area.appendChild(result);
+}
+
+function EmptyResults() {
+    const results = document.querySelectorAll('.result');
+    results.forEach(result => {
+    result.remove();
+    });
+}
+
+function KelvinToF(value) {
+    value = parseFloat(value);
+    return (((value-273.15)*1.8)+32).toFixed(1);
+}
+
+ function KelvinToC(value) {
+    value = parseFloat(value);
+    return (value-273.15).toFixed(1);
+}
+
+function RunJS(forc) {
+    let field = document.getElementById('input').value;
+    if (!field) {
+        return;
+    }
+    MainFunction(field, forc);
+}
